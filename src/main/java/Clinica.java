0f3f8c21ec789ff487b7056160757ede96dc8c3d
Thiago508 <santiago.trujillop@uqvirtual.edu.co>
2024-04-16 00:03:11 -0500

    Add CRUD method

--- src/main/java/Clinica.java
+++ src/main/java/Clinica.java
@@ -1,32 +1,34 @@
-import model.Cita;
-import model.LugarAtencion;
-import model.Medico;
-import model.Paciente;
-import utils.priorityQueue.PriorityQueue;
+import model.*;
+import utils.priorityQueue.CitaPriorityQueue;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Clinica {
 
     private static Clinica instance;
 
     private String nombre;
-    private PriorityQueue<Cita> citaPriorityQueue;
+    private CitaPriorityQueue citaPriorityQueue;
     private List<LugarAtencion> lugarAtencionList;
     private List<Paciente> pacienteList;
     private List<Medico> medicoList;
+    private List<GrupoPoblacional> grupoPoblacionalList;
 
-    private Clinica(String nombre, PriorityQueue<Cita> citaPriorityQueue, List<LugarAtencion> lugarAtencionList, List<Paciente> pacienteList, List<Medico> medicoList) {
+    private Clinica(String nombre) {
         this.nombre = nombre;
-        this.citaPriorityQueue = citaPriorityQueue;
-        this.lugarAtencionList = lugarAtencionList;
-        this.pacienteList = pacienteList;
-        this.medicoList = medicoList;
+        this.citaPriorityQueue = new CitaPriorityQueue();
+        this.lugarAtencionList = new ArrayList<>();
+        this.pacienteList = new ArrayList<>();
+        this.medicoList = new ArrayList<>();
+        this.grupoPoblacionalList = new ArrayList<>();
     }
 
-    public static synchronized Clinica getInstance(String nombre, PriorityQueue<Cita> citaPriorityQueue, List<LugarAtencion> lugarAtencionList, List<Paciente> pacienteList, List<Medico> medicoList) {
+    public static synchronized Clinica getInstance(String nombre) {
         if (instance == null) {
-            instance = new Clinica(nombre, citaPriorityQueue, lugarAtencionList, pacienteList, medicoList);
+            instance = new Clinica(nombre);
         }
         return instance;
     }
@@ -39,35 +41,122 @@ public class Clinica {
         this.nombre = nombre;
     }
 
-    public PriorityQueue<Cita> getCitaPriorityQueue() {
-        return citaPriorityQueue;
+    // CRUD Medico
+
+    public void addMedico(Medico medico){
+        medicoList.add(medico);
     }
 
-    public void setCitaPriorityQueue(PriorityQueue<Cita> citaPriorityQueue) {
-        this.citaPriorityQueue = citaPriorityQueue;
+    public Medico getMedico(String identificacion){
+        Optional<Medico> optionalMedico = medicoList
+                .stream()
+                .filter(medico -> medico.getIdentificacion().equals(identificacion))
+                .findFirst();
+
+        return optionalMedico.orElse(null);
     }
 
-    public List<LugarAtencion> getLugarAtencionList() {
-        return lugarAtencionList;
+    public List<Medico> getAllMedicos(){
+        return new ArrayList<>(medicoList);
     }
 
-    public void setLugarAtencionList(List<LugarAtencion> lugarAtencionList) {
-        this.lugarAtencionList = lugarAtencionList;
+    public void removeMedico(Medico medico){
+        medicoList.remove(medico);
+    }
+    public void updateMedico(Medico medico, Medico updateMedico){
+        medicoList.set(medicoList.indexOf(medico), updateMedico);
     }
 
-    public List<Paciente> getPacienteList() {
-        return pacienteList;
+    // CRUD Paciente
+
+    public void addPaciente(Paciente paciente){
+        pacienteList.add(paciente);
     }
 
-    public void setPacienteList(List<Paciente> pacienteList) {
-        this.pacienteList = pacienteList;
+    public Paciente getPaciente(String identificacion){
+        Optional<Paciente> optionalPaciente = pacienteList
+                .stream()
+                .filter(paciente -> paciente.getIdentificacion().equals(identificacion))
+                .findFirst();
+
+        return optionalPaciente.orElse(null);
     }
 
-    public List<Medico> getMedicoList() {
-        return medicoList;
+    public List<Paciente> getAllPacientes(){
+        return new ArrayList<>(pacienteList);
     }
 
-    public void setMedicoList(List<Medico> medicoList) {
-        this.medicoList = medicoList;
+    public void removePaciente(Paciente paciente){
+        pacienteList.remove(paciente);
     }
+    public void updatePaciente(Paciente paciente, Paciente updatePaciente){
+        pacienteList.set(pacienteList.indexOf(paciente), updatePaciente);
+    }
+
+    // CRUD Lugar atencion
+
+    public void addLugarAtencion(LugarAtencion lugarAtencion){
+        lugarAtencionList.add(lugarAtencion);
+    }
+
+    public LugarAtencion getLugarAtencion(String nombre){
+        Optional<LugarAtencion> optionalLugarAtencion = lugarAtencionList
+                .stream()
+                .filter(lugarAtencion -> lugarAtencion.getNombre().equals(nombre))
+                .findFirst();
+
+        return optionalLugarAtencion.orElse(null);
+    }
+
+    public List<LugarAtencion> getAllLugaresAtencion(){
+        return new ArrayList<>(lugarAtencionList);
+    }
+
+    public void removeLugarAtencion(LugarAtencion lugarAtencion){
+        lugarAtencionList.remove(lugarAtencion);
+    }
+    public void updateLugarAtencion(LugarAtencion lugarAtencion, LugarAtencion updateLugarAtencion){
+        lugarAtencionList.set(lugarAtencionList.indexOf(lugarAtencion), updateLugarAtencion);
+    }
+
+    // CRUD Grupo poblacional
+
+    public void addGrupoPoblacional(GrupoPoblacional grupoPoblacional){
+        grupoPoblacionalList.add(grupoPoblacional);
+    }
+
+    public GrupoPoblacional getGrupoPoblacional(String nombre){
+        Optional<GrupoPoblacional> optionalGrupoPoblacional = grupoPoblacionalList
+                .stream()
+                .filter(grupoPoblacional -> grupoPoblacional.getNombre().equals(nombre))
+                .findFirst();
+
+        return optionalGrupoPoblacional.orElse(null);
+    }
+
+    public List<GrupoPoblacional> getAllGruposPoblacionales(){
+        return new ArrayList<>(grupoPoblacionalList);
+    }
+
+    public void removeGrupoPoblacional(GrupoPoblacional grupoPoblacional){
+        grupoPoblacionalList.remove(grupoPoblacional);
+    }
+    public void updateGrupoPoblacional(GrupoPoblacional grupoPoblacional, GrupoPoblacional updateGrupoPoblacional){
+        grupoPoblacionalList.set(grupoPoblacionalList.indexOf(grupoPoblacional), updateGrupoPoblacional);
+    }
+
+    // CRUD Cita
+
+    public void addCita(Cita cita){
+        citaPriorityQueue.enqueue(cita);
+    }
+
+    public Cita getCita(){
+        return citaPriorityQueue.front();
+    }
+
+    public Cita attendCita(){
+        return citaPriorityQueue.unqueue();
+    }
+
 }
