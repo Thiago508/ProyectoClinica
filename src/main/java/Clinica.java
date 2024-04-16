f90ff7808e52edb7504a77dc16c777b49f8d6d01
Thiago508 <santiago.trujillop@uqvirtual.edu.co>
2024-04-16 16:01:20 -0500

    Organizate class

--- src/main/java/Clinica.java
+++ src/main/java/Clinica.java
@@ -4,7 +4,6 @@ import utils.priorityQueue.CitaPriorityQueue;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class Clinica {
 
