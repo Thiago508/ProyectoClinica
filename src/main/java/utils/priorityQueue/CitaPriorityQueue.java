22c0b175cc4a836f43028bc069af495ddc074742
Thiago508 <santiago.trujillop@uqvirtual.edu.co>
2024-04-16 00:02:37 -0500

    Complete unfinished classes

--- src/main/java/utils/priorityQueue/CitaPriorityQueue.java
+++ src/main/java/utils/priorityQueue/CitaPriorityQueue.java
@@ -1,17 +1,58 @@
 package utils.priorityQueue;
 
-public class PriorityQueue<T> {
+import model.Cita;
 
-    private Node<T> firstNode;
+public class CitaPriorityQueue {
+
+    private Node<Cita> firstNode;
     private int size;
 
-    public PriorityQueue(){
+    public CitaPriorityQueue(){
         this.firstNode = null;
         this.size = 0;
     }
 
-    public void enqueue(T value) {
+    public void enqueue(Cita value) {
+        Node<Cita> newNode = new Node<>(value);
 
+        Node<Cita> node = firstNode;
+        Node<Cita> nextNode = null;
+
+        while (value.getPaciente().getGrupoPoblacional().getPrioridad().getValue() < node.getValue().getPaciente().getGrupoPoblacional().getPrioridad().getValue()){
+            node = node.getNextNode();
+        }
+
+        nextNode = node.getNextNode();
+        newNode.setNextNode(nextNode);
+        node.setNextNode(newNode);
+
+        size++;
+    }
+
+    public Cita unqueue(){
+
+        if (isEmpty()){
+            return firstNode.getValue();
+        }
+
+        Node<Cita> node = firstNode;
+
+        firstNode = node.getNextNode();
+        size--;
+
+        return node.getValue();
+    }
+
+    public boolean isEmpty(){
+        return(firstNode == null);
+    }
+
+    public Cita front(){
+        return firstNode.getValue();
+    }
+
+    public int size(){
+        return size;
     }
 
 }
--- src/main/java/utils/priorityQueue/Node.java
+++ src/main/java/utils/priorityQueue/Node.java
@@ -1,4 +1,32 @@
 package utils.priorityQueue;
 
-public class Node {
+public class Node <T> {
+
+    private Node<T> nextNode;
+    private T value;
+
+    public Node(T value){
+        this.value = value;
+    }
+
+    public Node(T value, Node<T> nextNode){
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+
+    public Node<T> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
 }
